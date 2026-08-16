f6280c6cb3d97044b82864295b185c889c8153d6
John Engdal <john@engdal.fi>
2026-08-16 09:48:14 +0300

    project init

---
+++ devenv.nix
@@ -0,0 +1,58 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.nil
+    pkgs.statix
+    pkgs.nixfmt
+  ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    python = {
+      enable = true;
+      version = "3.14";
+      lsp.package = pkgs.basedpyright;
+      uv = {
+        enable = true;
+        package = pkgs.uv;
+
+        sync = {
+          enable = true;
+          allGroups = true;
+        };
+      };
+    };
+  };
+
+  scripts.manage.exec = ''
+    uv run src/manage.py "$@"
+  '';
+
+  scripts.web = {
+    exec = ''
+      uv run uvicorn --port 8000 --timeout-graceful-shutdown 0 --reload --reload-include *.css \
+              --reload-include *.js --reload-include *.html youtube_to_podcast.asgi:application
+    '';
+  };
+
+  processes.web = {
+    exec = ''
+      web
+    '';
+  };
+
+  # Avoid building the python package from sources:
+  cachix.pull = [ "nixpkgs-python" ];
+
+  # See full reference at https://devenv.sh/reference/options/
+}
