d3dd5d452ff8df6a4401f5a29c788f9edf0a53c3
John Engdal <john@engdal.fi>
2026-08-18 10:31:28 +0300

    Close the valkey glide client on shutdown

--- src/youtube_to_podcast/asgi.py
+++ src/youtube_to_podcast/asgi.py
@@ -1,16 +1,24 @@
-"""
-ASGI config for youtube_to_podcast project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_to_podcast.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "youtube_to_podcast.settings")
 
-application = get_asgi_application()
+_django_app = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    if scope["type"] == "lifespan":
+        while True:
+            message = await receive()
+            if message["type"] == "lifespan.startup":
+                await send({"type": "lifespan.startup.complete"})
+            elif message["type"] == "lifespan.shutdown":
+                # Add any global cleanup calls here.
+                from youtube_to_podcast import valkey_client
+
+                await valkey_client.close_client()
+                await send({"type": "lifespan.shutdown.complete"})
+                return
+    else:
+        await _django_app(scope, receive, send)
